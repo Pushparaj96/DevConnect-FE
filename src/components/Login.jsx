0c5c0678271b5ef41b2b5f3dd9e3fb399ab55d48
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-16 00:20:13 +0530

    login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,56 @@
-
-
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="navbar bg-base-300 p-5">
+        <a className="btn btn-ghost text-2xl hover:text-green-600">
+          DevConnect
+        </a>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="mt-[22%] sm:mt-[18%] md:mt-[10%] lg:mt-[6%] bg-base-300 p-8 rounded-xl w-8/12 sm:w-6/12 md:w-5/12 lg:w-1/4 shadow-md shadow-gray-600">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="text-xl font-medium flex items-center justify-center mb-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+              <span className="ms-1 text-2xl">Login</span>
+            </div>
+            <dl className="space-y-3">
+              <dt className="font-medium">Email ID</dt>
+              <dd>
+                <input
+                  type="text"
+                  className="input input-bordered input-primary w-full max-w-xs"
+                />
+              </dd>
+              <dt className="font-medium">Password</dt>
+              <dd>
+               <input
+                  type="text"
+                  className="input input-bordered input-primary w-full max-w-xs"
+                />
+              </dd>
+            </dl>
+              <div className="flex justify-center">
+              <button className="mt-8 bg-primary py-2 w-3/4 rounded-md text-lg font-medium hover:opacity-80 text-slate-950">LogIn</button>
+              </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Login;
