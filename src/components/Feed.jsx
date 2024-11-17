89e461c66ecb8acfc26cf942cad80d9fd957f855
Pushparaj96 <kpushparaj18@gmail.com>
2024-11-17 19:05:55 +0530

    added: feed data in redux store

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,31 @@
-
+import { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch , useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
+
+  const dispatch = useDispatch();
+  const feedData = useSelector(store=>store.feed);
+
+  const fetchFeed = async()=>{
+    if(feedData) return null;
+    try {
+      const response = await axios.get(`${BASE_URL}/user/feed`,{withCredentials:true});
+      dispatch(addFeed(response?.data?.data));
+      
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchFeed();
+  },[])
+
+
   return (
     <div>
         <h2>This is your Feed</h2>
